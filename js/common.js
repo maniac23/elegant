90612c0b157dd456d99b69e8eb7da73cc9bedfc0
Mikhail Tukai <tukaym@gmail.com>
2015-11-04 22:15:31 +0300

    first

---
+++ js/common.js
@@ -0,0 +1,104 @@
+$(document).ready(function () {
+
+    var MQL = 1170;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('.cd-header').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function () {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('.cd-header').hasClass('is-fixed')) {
+                        $('.cd-header').addClass('is-visible');
+                    } else {
+                        $('.cd-header').removeClass('is-visible is-fixed');
+                    }
+                } else {
+                    //if scrolling down...
+                    $('.cd-header').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('.cd-header').hasClass('is-fixed')) $('.cd-header').addClass('is-fixed');
+                }
+                this.previousTop = currentTop;
+            });
+    }
+
+    $('.cd-primary-nav-trigger').on('click', function () {
+        $('.cd-menu-icon').toggleClass('is-clicked');
+        $('.cd-header').toggleClass('menu-is-open');
+
+        //in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+        if ($('.cd-primary-nav').hasClass('is-visible')) {
+            $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').removeClass('overflow-hidden');
+            });
+        } else {
+            $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function () {
+                $('body').addClass('overflow-hidden');
+            });
+        }
+    });
+
+    //Цели для Яндекс.Метрики и Google Analytics
+    $(".count_element").on("click", (function () {
+        ga("send", "event", "goal", "goal");
+        yaCounterXXXXXXXX.reachGoal("goal");
+        return true;
+    }));
+
+    //SVG Fallback
+    if (!Modernizr.svg) {
+        $("img[src*='svg']").attr("src", function () {
+            return $(this).attr("src").replace(".svg", ".png");
+        });
+    };
+    // gallery
+    $("#gallery").justifiedGallery({
+        rowHeight: 300,
+        margins: 3,
+        lastRow: 'hide'
+    });
+
+    //Аякс отправка форм
+    //Документация: http://api.jquery.com/jquery.ajax/
+    $("#form").submit(function () {
+        $.ajax({
+            type: "POST",
+            url: "mail.php",
+            data: $(this).serialize()
+        }).done(function () {
+            alert("Спасибо за заявку!");
+            setTimeout(function () {
+
+                $("#form").trigger("reset");
+            }, 1000);
+        });
+        return false;
+    });
+
+    //Chrome Smooth Scroll
+    try {
+        $.browserSelector();
+        if ($("html").hasClass("chrome")) {
+            $.smoothScroll();
+        }
+    } catch (err) {
+
+    };
+
+    $("img, a").on("dragstart", function (event) {
+        event.preventDefault();
+    });
+
+});
+
+$(window).load(function () {
+
+    $(".loader_inner").fadeOut();
+    $(".loader").delay(400).fadeOut("slow");
+
+});
